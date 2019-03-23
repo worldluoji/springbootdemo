7746e5331be7ccd3f5f12943dc250d3a4d93ba45
worldluoji <qq1132416@outlook.com>
2019-03-23 22:39:31 +0800

    import lombok for spring login demo

--- srpintbootlogindemo/src/main/java/com/example/srpintbootlogindemo/Models/User.java
+++ srpintbootlogindemo/src/main/java/com/example/srpintbootlogindemo/Models/User.java
@@ -1,11 +1,20 @@
 package com.example.srpintbootlogindemo.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
@@ -13,46 +22,4 @@ public class User {
     private String name;
     private String email;
     private String password;
-
-    @Override
-    public String toString() {
-        return "com.example.srpintbootlogindemo.Models.User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
