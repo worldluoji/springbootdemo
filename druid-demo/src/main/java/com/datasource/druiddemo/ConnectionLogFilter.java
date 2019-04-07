79bca92d6a4ffb5517b93ef46797ed494994ea85
worldluoji <qq1132416@outlook.com>
2019-04-07 21:03:29 +0800

    add alibaba druid demo

---
+++ druid-demo/src/main/java/com/datasource/druiddemo/ConnectionLogFilter.java
@@ -0,0 +1,21 @@
+package com.datasource.druiddemo;
+
+import com.alibaba.druid.filter.FilterChain;
+import com.alibaba.druid.filter.FilterEventAdapter;
+import com.alibaba.druid.proxy.jdbc.ConnectionProxy;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Properties;
+
+@Slf4j
+public class ConnectionLogFilter extends FilterEventAdapter {
+    @Override
+    public void connection_connectBefore(FilterChain chain, Properties info) {
+        System.out.println("Before connection");
+    }
+
+    @Override
+    public void connection_connectAfter(ConnectionProxy connection) {
+        System.out.println("After connection");
+    }
+}
---
+++ druid-demo/src/main/java/com/datasource/druiddemo/DruidDemoApplication.java
@@ -0,0 +1,33 @@
+package com.datasource.druiddemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@SpringBootApplication
+@Slf4j
+public class DruidDemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidDemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Data source is {}", dataSource.toString());
+        List<String> bars =  jdbcTemplate.queryForList("SELECT bar FROM Foo", String.class);
+        bars.forEach(s -> log.info(s));
+    }
+}
