71a4b3d0dfc0a274792d1025736a960b3140f5ff
worldluoji <qq1132416@outlook.com>
2019-05-01 18:16:06 +0800

    加入feign接口调用日志打印和feign的ribbon超时时间、超时重试等

--- feign-hello/feign-consumer/src/main/java/com/feign/feignconsumer/service/ProductService.java
+++ feign-hello/feign-consumer/src/main/java/com/feign/feignconsumer/service/ProductService.java
@@ -1,9 +1,10 @@
 package com.feign.feignconsumer.service;
 
+import com.feign.feignconsumer.feignconfig.FeignClientConfig;
 import com.feign.feignprovider.facade.ProductFacade;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name="e-book-product")
+@FeignClient(name="e-book-product", configuration = FeignClientConfig.class)
 public interface ProductService extends ProductFacade {
 	
 }
