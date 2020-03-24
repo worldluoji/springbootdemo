2dfb770add499e21fb2f40684c988b67d44f3a2f
worldluoji <qq1132416@outlook.com>
2020-03-24 11:03:49 +0800

    【修改内容】：用Zuul做校验 【修改人】：luoji 【修改时间】：2020/03/24

--- springcloud-zuul-demo/simple-zuul-demo/src/main/java/com/springboot/zuul/demo/simplezuuldemo/filters/PreFilter.java
+++ springcloud-zuul-demo/simple-zuul-demo/src/main/java/com/springboot/zuul/demo/simplezuuldemo/filters/PreFilter.java
@@ -21,14 +21,23 @@ public class PreFilter extends ZuulFilter {
 
     @Override
     public boolean shouldFilter() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info("Request Method : " + request.getMethod() + " Request URL : " + request.getRequestURL().toString());
+        // 这个是获取?name=luoji的参数
+        String name = request.getParameter("name");
+        if (name == null || name.isEmpty()) {
+            log.info("用户验证不通过");
+            // 实际系统中做验证，是远程调用到认证系统去做认证，这里只是简单模拟一下
+            return false;
+        }
+        log.info("登录的用户：" + name);
         return true;
     }
 
     @Override
     public Object run() throws ZuulException {
-        RequestContext ctx = RequestContext.getCurrentContext();
-        HttpServletRequest request = ctx.getRequest();
-        log.info("Request Method : " + request.getMethod() + " Request URL : " + request.getRequestURL().toString());
+        log.info("shouldFilter为true，才会执行下run方法");
         return null;
     }
 }
