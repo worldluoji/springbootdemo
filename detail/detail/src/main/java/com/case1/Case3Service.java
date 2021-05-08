e08db420e92b86819005a624946ce13badecc5ea
worldluoji <qq1132416@outlook.com>
2021-05-08 15:29:42 +0800

    @prototype issue@luoji@20210508

---
+++ detail/detail/src/main/java/com/case1/Case3Service.java
@@ -0,0 +1,16 @@
+package com.case1;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class Case3Service {
+    public void say() {
+        log.info("i am a bean which type if prototype");
+    }
+}
---
+++ detail/detail/src/main/java/com/example/detail/controllers/HelloController.java
@@ -0,0 +1,39 @@
+package com.example.detail.controllers;
+
+import com.case1.Case3Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private Case3Service case3;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    /**
+    * 不管我们访问多少次http://localhost:8080/hello，访问的结果都是不变的,
+    * 这与Case3Service prototype设定背道而驰
+    **/
+    @GetMapping(path="hello")
+    public String hello() {
+        return "hello : " + case3;
+    }
+
+    /**
+    * 这种方式可以解决上面的问题
+    **/
+    @GetMapping(path = "hi")    
+    public String hi() {
+        return "helloworld, service is : " + getServiceImpl();    
+    };
+
+    public Case3Service getServiceImpl() {        
+        return applicationContext.getBean(Case3Service.class);    
+    }
+}
