ad15efb0ee3303c9a2996391059c2c34fc226d8c
worldluoji <qq1132416@outlook.com>
2021-06-09 09:30:05 +0800

    @Autowired补充@luoji@20210609

--- detail/detail/src/main/java/com/example/detail/DetailApplication.java
+++ detail/detail/src/main/java/com/example/detail/DetailApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 
