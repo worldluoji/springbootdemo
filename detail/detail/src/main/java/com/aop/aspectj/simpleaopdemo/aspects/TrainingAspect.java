9566789be1343cfb8896b0a2920cb7cb1d5ea131
worldluoji <qq1132416@outlook.com>
2021-06-21 19:50:09 +0800

    @aop@luoji@20210621

--- detail/detail/src/main/java/com/aop/aspectj/simpleaopdemo/aspects/TrainingAspect.java
+++ detail/detail/src/main/java/com/aop/aspectj/simpleaopdemo/aspects/TrainingAspect.java
@@ -3,7 +3,10 @@ package com.aop.aspectj.simpleaopdemo.aspects;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 
+import lombok.extern.slf4j.Slf4j;
+
 // 定义一个切面
+@Slf4j
 @Aspect
 public class TrainingAspect {
     // 指定切点(什么时候会进行拦截)，execution表示执行方法train()时会执行， *表示任意返回类型的方法，..代表任意参数,
@@ -21,12 +24,17 @@ public class TrainingAspect {
     // 以下是四种通知
     @Before("doTrain()")
     public void before() {
-       System.out.println("before train...");
+       log.info("before train...");
+    }
+
+    @Before("doTrain()")
+    public void bef() {
+        log.info("bef train...");
     }
 
     @After("doTrain()")
     public void after() {
-       System.out.println("after train...");
+        log.info("after train...");
     }
 
     // 环绕通知可以同时进行前置和后置通知，当然也可以再jp.proceed()执行前return中止后续操作
@@ -38,29 +46,29 @@ public class TrainingAspect {
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
-        System.out.println("运动后要补充水分并进行肌肉拉伸");
+        log.info("运动后要补充水分并进行肌肉拉伸");
     }
 
     @AfterReturning("doTrain()")
     public void afterReturning() {
-        System.out.println("运动全部完成");
+        log.info("运动全部完成");
     }
 
     @AfterThrowing("doTrain()")
     public void afterThrowing() {
-        System.out.println("今天状态不好，天气炎热，运动终止");
+        log.warn("今天状态不好，天气炎热，运动终止");
     }
 
     //jp一定更要写在第一个参数，否则会报illegalStateException
     @Around("doTrainWithMate(mateName)")
     public void aroundWithMate(ProceedingJoinPoint jp, String mateName) {
-        System.out.println(mateName + "是新手，记得给她买水...");
+        log.info(mateName, "是新手，记得给她买水...");
         try {
             jp.proceed();
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
-        System.out.println("运动结束，带" + mateName + "去吃顿大餐");
+        log.info("运动结束，带", mateName, "去吃顿大餐");
     }
 
 }
