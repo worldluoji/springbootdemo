1950538f7d340fbc70f35f1e8429a77eb546ad5a
worldluoji <qq1132416@outlook.com>
2021-06-22 19:08:47 +0800

    @DateFormat@luoji@20210622

--- detail/detail/src/main/java/com/example/detail/controllers/UrlParseController.java
+++ detail/detail/src/main/java/com/example/detail/controllers/UrlParseController.java
@@ -1,5 +1,8 @@
 package com.example.detail.controllers;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +17,15 @@ public class UrlParseController {
     @GetMapping(path = "/p/hi2")
     public String hi2(@RequestParam String name){
         return name;
+    };
+
+    @GetMapping(path = "/p/hi6")
+    public String hi6(@RequestParam("Date") Date date){
+        return "date is " + date ;
+    };
+
+    @GetMapping(path = "/p/hi7")
+    public String hi7(@RequestParam("Date") @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") Date date){
+        return "date is " + date ;
     };
 }
