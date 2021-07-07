4d3a5c7abf02eed9543c0faa8dee313c94f000f7
worldluoji <qq1132416@outlook.com>
2021-07-07 17:24:58 +0800

    @suggester@luoji@20210707

--- eslearn/search-demo/demo/src/main/java/com/es/demo/controllers/SuggesterController.java
+++ eslearn/search-demo/demo/src/main/java/com/es/demo/controllers/SuggesterController.java
@@ -1,8 +1,24 @@
 package com.es.demo.controllers;
 
+import java.util.List;
+
+import com.es.demo.models.SearchRequest;
+import com.es.demo.services.SuggesterService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SuggesterController {
-    
+
+    @Autowired
+    private SuggesterService suggesterService;
+
+    @PostMapping(path="suggest")
+    public List<String> getSuggestedWords(@RequestBody @Validated SearchRequest request) {
+        return suggesterService.getSuggestedWords(request.getKeyword());
+    }
 }
