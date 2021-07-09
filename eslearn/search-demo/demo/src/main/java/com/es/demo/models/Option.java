aabbde80e1211f677456865aeedb0c210bce1f53
worldluoji <qq1132416@outlook.com>
2021-07-09 17:38:56 +0800

    @去重@luoji@20210709

--- eslearn/search-demo/demo/src/main/java/com/es/demo/models/Option.java
+++ eslearn/search-demo/demo/src/main/java/com/es/demo/models/Option.java
@@ -10,5 +10,30 @@ import lombok.NoArgsConstructor;
 public class Option {
     private String text;
     private double score;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Option other = (Option) obj;
+        if (text == null) {
+            if (other.text != null)
+                return false;
+        } else if (!text.equals(other.text))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((text == null) ? 0 : text.hashCode());
+        return result;
+    }
     
 }
